13363fc2f17dc428d4fccad5895ecb5139ecc1c5
ParthHonrao05 <pyhonrao5327@gmail.com>
2026-04-08 01:42:08 +0530

    final

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://aqi-console.vercel.app/",
+  origin: "https://localhost:3000",
   credentials: true
 }));
 app.use(express.json());
